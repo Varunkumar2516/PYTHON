d8f2946f0c89bfbd65cf69383324ba0a765694e5
VermaVarun143 <kavitarani8289@gmail.com>
2025-06-21 23:11:06 +0530

    DAY 8

--- DATA STRUCTURES/DAY 8/Practice/problem1.py
+++ DATA STRUCTURES/DAY 8/Practice/problem1.py
@@ -3,8 +3,15 @@ students = {
     "B": {"name": "Emma"},
     "C": {"grade": "A"},
 }
+print("\n\n method 1")
 #Fetch data from the nested dictinary without error
 for key, data in students.items():
     name = data.get("name", "Not Provided")
     grade = data.get("grade", "Not Available")
     print(f"{key} => Name: {name}, Grade: {grade}")
+
+print("\n\n method 2")
+for student,info in students.items():
+    print(student)
+    for i,j in info.items():
+        print(f"{i}:{j}")
